34539f1ca1eaa73ff6bab1675aa7309e36dec1bd
OmegaToken <70204881+OmegaToken@users.noreply.github.com>
2020-08-25 13:10:03 +0200

    Create OmegaToken.sol

---
+++ OmegaToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20Burnable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20Pausable.sol";
+
+contract OmegaToken is ERC20, ERC20Detailed, ERC20Burnable, ERC20Mintable, ERC20Pausable
+{
+    constructor () public ERC20Detailed("OmegaToken", "OMT", 0)
+    {
+        _mint(msg.sender, 1000000000 * (10 ** uint256(decimals())));
+    }
+}
