e29979a7c85ad726a86b9bb3642425af6e9c7899
OmegaToken <70204881+OmegaToken@users.noreply.github.com>
2020-08-26 11:53:05 +0200

    Update Omega.sol

--- Omega.sol
+++ Omega.sol
@@ -6,7 +6,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20Pausable.sol";
 
-contract OmegaToken is ERC20, ERC20Detailed, ERC20Burnable, ERC20Mintable, ERC20Pausable
+contract Omega is ERC20, ERC20Detailed, ERC20Burnable, ERC20Mintable, ERC20Pausable
 {
     constructor () public ERC20Detailed("Omega", "OMEGA", 2)
     {
